c4903170e60be652455245563b291c530647b045
pushpakagarwal1702 <sumitraagarwal205@gmail.com>
2026-03-02 09:00:21 +0530

    Add files via upload

---
+++ uc5.java
@@ -0,0 +1,21 @@
+
+public class UC5 {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            "    ***        ***      ******      *****  ",  
+            "  **   **    **   **    **   **   **       ",   
+            " **     **  **     **   **   **  ***       ",  
+            " **     **  **     **   ******     *****   ", 
+            " **     **  **     **   **              ** ",  
+            "  **   **    **   **    **       **     ** ",   
+            "    ***        ***      **         *****   "   
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
+
